50a30a4c4d07e800546269ce3d8463973b4efa5a
Achraf Bardan <achrafbardan@gmail.com>
2024-04-27 19:29:08 +0200

    feat:  add config and register SimpleIdRegistrar

---
+++ config/simple-id.php
@@ -0,0 +1,9 @@
+<?php
+
+return [
+    'salt' => env('SIMPLE_ID_SALT', 'simple-id'),
+
+    'models' => [
+        // 'users' => \App\Models\User::class,
+    ],
+];
---
+++ src/SimpleIdServiceProvider.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AchrafBardan\SimpleId;
+
+use Illuminate\Support\ServiceProvider;
+
+class SimpleIdServiceProvider extends ServiceProvider
+{
+    /**
+     * Boot method of this service provider.
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        $this->publishes([
+            __DIR__.'/../config/simple-id.php' => config_path('simple-id.php'),
+        ]);
+
+        $this->app->make(SimpleIdRegistrar::class)->registerModels(config('simple-id.models'));
+    }
+
+    /**
+     * Register method of this service provider.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        $this->mergeConfigFrom(
+            __DIR__.'/../config/simple-id.php',
+            'simple-id'
+        );
+
+        $this->app->bind(SimpleIdRegistrar::class, function () {
+            return new SimpleIdRegistrar();
+        });
+    }
+}
