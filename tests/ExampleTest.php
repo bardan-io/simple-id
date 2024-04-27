5a9303ba3fae62301ec3e6a7ff8af631ba80650e
Achraf Bardan <achrafbardan@gmail.com>
2024-04-27 19:27:48 +0200

    feat:  set up tests

---
+++ tests/ExampleTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace AchrafBardan\SimpleId\Tests;
+
+class ExampleTest extends TestCase
+{
+    public function testExample()
+    {
+        $this->assertTrue(true);
+    }
+}
---
+++ tests/TestCase.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace AchrafBardan\SimpleId\Tests;
+
+class TestCase extends \Orchestra\Testbench\TestCase
+{
+    //
+}
